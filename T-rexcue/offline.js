9711662af5be40c7003fea8d32dc6aff50d8f27b
tiagop85 <tiagoparente85@gmail.com>
2017-01-30 15:20:43 -0400

    T-rexcue

---
+++ T-rexcue/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1485803979,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/spandaime-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sptamboresq-sheet0.png",
+		"images/sptempstart-sheet0.png",
+		"images/spjogador-sheet0.png",
+		"images/sp-sheet0.png",
+		"images/sp-sheet1.png",
+		"images/sp-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
